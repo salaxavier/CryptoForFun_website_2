48905469578b4b91c941eaec17fac079f2fa4cd9
salaxavier <salaxavier@outlook.com>
2019-04-05 03:06:09 +0100

    login successful

--- cryptoforfun/routes/users.js
+++ cryptoforfun/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
 
@@ -56,4 +58,37 @@ router.get('/login', function(req, res) {
   //res.sendfile('login.html');
 });
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getUserByUsername(username, function(err, user){
+      if(err) throw err;
+      if(!user){
+        return done(null, false, {message: 'Invalid Username or Password'});
+      }
+      User.comparePassword(password, user.password, function(err, isMatch){
+        if(err) throw err;
+        if(isMatch){
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Invalid Username or Password'});
+        }
+      });
+    });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+  passport.authenticate('local',{successRedirect:'/', failureRedirect:'/users/login', failureFlash: true}),
+  function(req, res) {
+    res.redirect('/');
+  });
 module.exports = router;
